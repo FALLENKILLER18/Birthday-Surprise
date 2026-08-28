9f9679df02696cea6b736081015b02e405f0cb4b
FALLENKILLER18 <fallenkiller18@gmail.com>
2026-08-28 10:38:06 +0800

    Add birthday check functionality in script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+function checkBirthday() {
+
+    // CHANGE THIS LATER TO HER BIRTHDAY
+    const correctBirthday = "2000-08-28";
+
+    // Get the birthday entered by the user
+    const enteredBirthday = document.getElementById("birthday").value;
+
+    const result = document.getElementById("result");
+
+    // Check if the birthday is correct
+    if (enteredBirthday === correctBirthday) {
+
+        result.innerHTML = `
+            🎉 It's your birthday! 🎉
+            <br><br>
+            I have a little surprise waiting for you... ❤️
+            <br><br>
+            <button onclick="continueToSurprise()">
+                CONTINUE ❤️
+            </button>
+        `;
+
+    } else {
+
+        result.innerHTML = `
+            ❌ Hmm... that's not the date I'm looking for.
+            <br>
+            Try again ❤️
+        `;
+
+    }
+}
+
+
+function continueToSurprise() {
+
+    alert("Our birthday surprise is coming soon! ❤️");
+
+}
